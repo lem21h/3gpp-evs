9fb099f8018e5900af1d645e796dfb6f0ab2ec8d
konrad <konradend@gmail.com>
2025-02-10 13:09:20 +0400

    Update to 05.2024 version

---
+++ lib_com/disclaimer.h
@@ -0,0 +1,15 @@
+/*====================================================================================
+    EVS Codec 3GPP TS26.442 Nov 04, 2021. Version 12.15.0 / 13.10.0 / 14.6.0 / 15.4.0 / 16.4.0
+  ====================================================================================*/
+
+#ifndef __INCLUDED_DISCLAIMER_H
+#define __INCLUDED_DISCLAIMER_H
+
+#include <stdio.h>
+
+int print_disclaimer(FILE *fPtr);
+
+
+
+
+#endif /* __INCLUDED_DISCLAIMER_H */
